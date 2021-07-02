70600fd6dc365316e963bb31818ecec2981eb0d4
LePhuc-Xuong <86821383+LePhuc-Xuong@users.noreply.github.com>
2021-07-02 13:31:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Bé Xuồng à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
+    Mà sao em cứ đi đúng đường gòi
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    title: 'Phải chăng em đã yêu ngay từ cái mặt đẹp troai 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười anh ',
+    btnYes: 'Em yêu anh vô cùng luôn LHP <33
+    btnNo: 'Không, anh đừng có mơ :3',',
+    question:'Trên thế giới có rất nhiều anh đẹp trai mà em lại yêu anh',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Yêu thì yêu không thì vẫn rất yêu anh <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    messDesc: 'Tối nay 5h hẹn em học anh văn vs anh.',
+    btnAccept: 'Oki anh yêuu <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
